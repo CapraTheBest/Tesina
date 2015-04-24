870e1266766d4f5c61fbd9b10c99c2bf4581c25b
CapraTheBest <gianluca.beladirr2y@gmail.com>
2015-04-24 10:58:12 +0200

    Added .gitignore, added java project for reading..

---
+++ Java/Serial/src/me/belardinelli/tesina/serial/Main.java
@@ -0,0 +1,20 @@
+package me.belardinelli.tesina.serial;
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SerialPort serialPort = new SerialPort("COM1");
+        try {
+            serialPort.openPort();//Open serial port
+            serialPort.setParams(9600, 8, 1, 0);//Set params.
+            byte[] buffer = serialPort.readBytes(10);//Read 10 bytes from serial port
+            serialPort.closePort();//Close serial port
+        }
+        catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
+    }
+}
