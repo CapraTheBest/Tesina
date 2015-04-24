ae4c9605cc83d6a7d95e8e8371815bec82c83671
CapraTheBest <gianluca.beladirr2y@gmail.com>
2015-04-24 10:59:45 +0200

    .. serial and modified a bit of the php
    I'm super duper late :scream:

--- Php/File_test.php
+++ Php/File_test.php
@@ -1,6 +1,7 @@
 <?php
 $h = "25.05";
-$t = "20.00"; 
+$t = "20.00";
+$date = date('Y/m/d h:i:s', time());
 
 $connection = mysql_connect("localhost","root","");
 if (!$connection) {
@@ -10,12 +11,26 @@ echo 'Connected successfully';
 
 mysql_select_db("pianta",$connection);
 
+$handle = @fopen("newfile.txt", "r");
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        echo $buffer;
+    }
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($handle);
+}
+
+/* write shit yoh
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 $txt = $h."\n";
 fwrite($myfile, $txt);
 $txt = $t."\n";
 fwrite($myfile, $txt);
-fclose($myfile);
+$txt = $date."\n";
+fwrite($myfile, $txt);
+fclose($myfile);*/
 
 // Read file
 $myfile = fopen("newfile.txt", "r") or die("Unable to open file!");
@@ -24,7 +39,7 @@ while(!feof($myfile)) {
   echo fgets($myfile) . "<br>";
 }
 fclose($myfile);
-$query = "INSERT INTO `pianta`.`pianta` (`id`, `time`, `humidity`, `temperature`) VALUES (NULL, CURRENT_TIMESTAMP, '".$h."', '".$t."')";
+$query = "INSERT INTO `pianta`.`pianta` (`id`, `time`, `humidity`, `temperature`) VALUES (NULL, '".$date."', '".$h."', '".$t."')";
   $result = mysql_query($query,$connection);
   echo $result;
 
